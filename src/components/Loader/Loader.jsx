8f2b93ba126e884d609789381026bc21c6713417
Mika Otter <mi.otter.side@gmail.com>
2024-05-09 17:34:25 +0200

    Done : Counter

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,17 +1,24 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect, forwardRef } from "react";
 import s from "./Loader.module.scss";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { LogoStartSVG } from "../SVG/LogoStartSVG";
+import cn from "classnames";
 
 gsap.registerPlugin();
 
 export default function Loader() {
     const bigBoxRef = useRef();
     const smallBoxRef = useRef();
+    const counterOneRef = useRef();
+    const counterTwoRef = useRef();
+    const counterThreeRef = useRef();
     const rectsRef = useRef([]);
     const [enter, setEnter] = useState(false);
     const [animation, setAnimation] = useState(true);
+    const counterOne = [0, 1];
+    const counterTwo = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    const counterThree = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
     function shuffleArray(array) {
         for (let i = array.length - 1; i > 0; i--) {
@@ -92,13 +99,64 @@ export default function Loader() {
                     <div className={s.loader__box__ctn}>
                         <LogoStartSVG rectsRef={rectsRef} />
                     </div>
+                    <div className={s.loaderbar}>
+                        <div className={s.loaderbar__one}></div>
+                    </div>
+
+                    <div className={s.counter__box}>
+                        <Counter initialNumber={0} duration={1} delay={4} numbers={counterOne} />
+                        <Counter initialNumber={0} duration={6} delay={0} numbers={counterTwo} />
+                        <Counter initialNumber={1} duration={2} delay={3} numbers={counterThree} />
+                    </div>
                     {!animation ? (
-                        <button type="button" onClick={() => handleEnter()}>
-                            ENTER
-                        </button>
+                        <>
+                            <button type="button" onClick={() => handleEnter()}>
+                                ENTER
+                            </button>
+                            <h2>REMI CROCE</h2>
+                            <h2>CREATIVE DEVELOPER</h2>
+                        </>
                     ) : null}
                 </div>
             </section>
         </>
     );
 }
+
+const CounterNumber = forwardRef(({ number }, ref) => {
+    return (
+        <div className={s.num} ref={ref}>
+            {number}
+        </div>
+    );
+});
+
+const Counter = ({ initialNumber, duration, delay, numbers }) => {
+    const counterRef = useRef();
+    const numRefs = useRef([]);
+    const [counterNumbers, setCounterNumbers] = useState(numbers);
+
+    useEffect(() => {
+        const numHeight = numRefs.current[0]?.clientHeight;
+        const totalDistance = (counterNumbers.length - 1) * numHeight;
+
+        gsap.to(counterRef.current, {
+            y: -totalDistance,
+            duration: duration,
+            delay: delay,
+            ease: "power2.inOut",
+        });
+    }, []);
+
+    return (
+        <div className={s.counter} ref={counterRef}>
+            {counterNumbers.map((number, index) => (
+                <CounterNumber
+                    key={index}
+                    number={number}
+                    ref={(el) => (numRefs.current[index] = el)}
+                />
+            ))}
+        </div>
+    );
+};
