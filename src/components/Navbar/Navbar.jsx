33c15d207abbf36fd24ba22f6eaf0f04bc200c2e
Mika Otter <mi.otter.side@gmail.com>
2024-05-10 13:02:50 +0200

    Done : Adjust Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,19 +12,30 @@ import AudioPlayer from "../Audio/Audio";
 export default function Navbar({ activeSound, handleMenu }) {
     return (
         <div className={s.navbar}>
-            <div className={s.navbar__menu}>
-                <span onClick={() => handleMenu()}>MENU</span>
-                {/* <div className={s.navbar__menu__icone}>
+            <div className={s.navbar__rotator}>
+                <div className={s.navbar__ctn}>
+                    <div className={s.navbar__ctn__wrapper} onClick={() => handleMenu()}>
+                        <div className={s.navbar__ctn__button}>
+                            <span>MENU</span>
+                            <span>MENU</span>
+                        </div>
+                        {/* <div className={s.navbar__menu__icone}>
                     <MenuIconSVG />
                 </div> */}
-            </div>
-            <div className={s.navbar__letstalk}>
-                <span>CONTACT </span>
-                <div className={s.navbar__letstalk__icone}>{/* <ArrowLetsTalkSVG /> */}</div>
-                <span>/</span>
-            </div>
-            <div className={s.navbar__sound}>
-                <AudioPlayer activeSound={activeSound} />
+                    </div>
+                    <div className={s.navbar__ctn__separator}>
+                        <span>/</span>
+                    </div>
+                    <div className={s.navbar__ctn__wrapper}>
+                        <div className={s.navbar__ctn__button}>
+                            <span>CONTACT </span>
+                            <span>CONTACT </span>
+                        </div>
+                    </div>
+                </div>
+                <div className={s.navbar__sound}>
+                    <AudioPlayer activeSound={activeSound} />
+                </div>
             </div>
         </div>
     );
