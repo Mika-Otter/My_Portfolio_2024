b52d1b5f3c3b89058b9c21d3abfba24f39de317c
Mika Otter <mi.otter.side@gmail.com>
2024-05-25 16:32:04 +0200

    DEBUG

---
+++ frontend/src/components/Projects/ProjectsData.js
@@ -0,0 +1,124 @@
+const projectsData = [
+    // {
+    //     title: "Antoine Lichtenberg ",
+    //     smalltext: "  Decorator",
+    //     year: "2024",
+    //     url: "./fake-project-archi.png",
+    //     alt: "Antoine Lichtenberg",
+    //     text:
+    //         "I created a portfolio for Antoine Lichtenberg, a former architect who is now moving" +
+    //         " into film decoration. I enjoyed continuing to explore GSAP and responding to a first concrete project," +
+    //         " for a client who had a wish and desires that I had to follow. I'm very excited to be working for inspiring people. ",
+    //     techno: "React, GSAP",
+    //     client: "Antoine Lichtenberg Decorator",
+    //     type: "website",
+    // },
+    // {
+    //     title: "Booball ",
+    //     smalltext: "  Small Game with a boo",
+    //     year: "2024",
+    //     url: "./fake-project-booball.png",
+    //     alt: "Booball",
+    //     text:
+    //         "When I learnt about canvas elements in html, I made this little animation." +
+    //         " Basically just a decorative element. Later, I had the idea of turning it into a simple, fun game.",
+    //     techno: "Javascript POO, canvas html element",
+    //     client: "Personnal",
+    //     type: "game",
+    // },
+    {
+        title: "DNSEP2021 ",
+        smalltext: "  Art diploma exhibition",
+        year: "2024",
+        url: "./src/assets/video/DNSEP.mp4",
+        alt: "DNSEP2021",
+        text:
+            " DNSEP 2021 is a website who summarize my practice in art school. " +
+            "More precisely my exibition-diploma in June 2021." +
+            " I had fun to use GSAP and have again a lot of idea for improve that. Currently stopped" +
+            "for progress with other sites.",
+        techno: "React, GSAP",
+        client: "Personnal",
+        type: "website",
+        img1: "./src/assets/img/project-dnsep/dnsep-1.webp",
+        img2: "./src/assets/img/project-dnsep/dnsep-2.webp",
+        img3: "./src/assets/img/project-dnsep/dnsep-3.webp",
+        img4: "./src/assets/img/project-dnsep/dnsep-4.webp",
+        img5: "./src/assets/img/project-dnsep/dnsep-5.webp",
+        img6: "./src/assets/img/project-dnsep/dnsep-6.webp",
+        img7: "./src/assets/img/project-dnsep/dnsep-7.webp",
+        img8: "./src/assets/img/project-dnsep/dnsep-8.webp",
+    },
+    {
+        title: "Typpov ",
+        smalltext: "  Ri7 Final Exam Project, typographie tool",
+        year: "2024",
+        url: "./src/assets/video/Typpov-Low.mp4",
+        alt: "Typpov",
+        text:
+            " I had to do a project for my degree at Ri7. I was engulfed in a multitude of folders where I stored" +
+            " all my typefaces. But it was a bloody mess. So I had the idea of developing a tool to classify fonts. And" +
+            " also to test them from different angles. You can create an account, log in and upload your favourite" +
+            " fonts. I still have many ideas for features to develop, but for the moment I'm concentrating on my projects in Three.js. ",
+        techno: "Figma, React, GSAP, Node.js, MySQL, Prisma, JWT",
+        client: "School Project",
+        type: "website",
+        img1: "./src/assets/img/project-typpov/inprogress.png",
+        img2: "./src/assets/img/project-typpov/inprogress.png",
+        img3: "./src/assets/img/project-typpov/inprogress.png",
+        img4: "./src/assets/img/project-typpov/inprogress.png",
+        img5: "./src/assets/img/project-typpov/inprogress.png",
+        img6: "./src/assets/img/project-typpov/inprogress.png",
+        img7: "./src/assets/img/project-typpov/inprogress.png",
+        img8: "./src/assets/img/project-typpov/inprogress.png",
+    },
+
+    {
+        title: "Platform Game ",
+        smalltext: "  Personal Game",
+        year: "2023",
+        url: "./src/assets/video/Platform.mp4",
+        alt: "Platform Game",
+        text: "While learning OOP in JS, I created a little platform game. I learned a lot thanks to Frank Laboratory on youtube. ",
+        techno: "Javascript POO, canvas html element",
+        client: "Personnal Prototype",
+        type: "game",
+    },
+    {
+        title: "UNEXPECTED Studio ",
+        smalltext: "  Design",
+        year: "2023",
+        url: "./src/assets/video/UnexpectedStudio.mp4",
+        alt: "UNEXPECTEDStudio",
+        text:
+            "I produced a proposal for the redesign of the Unexpected Studio website. I followed their graphic guidelines" +
+            " to come up with something that suited their world and the market. I loved working on a real project.  ",
+        techno: "Figma, Adobe Illustrator, Adobe Photoshop",
+        client: "Personnal project to be sent to UNEXPECTEDStudio ",
+        type: "website",
+        img1: "./src/assets/img/project-unexpected/unexpected3.webp",
+        img2: "./src/assets/img/project-unexpected/unexpected2.webp",
+        img3: "./src/assets/img/project-unexpected/unexpected7.webp",
+        img4: "./src/assets/img/project-unexpected/unexpected4.webp",
+        img5: "./src/assets/img/project-unexpected/unexpected5.webp",
+        img6: "./src/assets/img/project-unexpected/unexpected8.webp",
+        img7: "./src/assets/img/project-unexpected/unexpected6.webp",
+        img8: "./src/assets/img/project-unexpected/unexpected1.webp",
+    },
+    {
+        title: "Various Game ",
+        smalltext: "  Morpion, Puissance4, Pendu, Labyrinth",
+        year: "2023",
+        url: "./src/assets/video/VariousGame.mp4",
+        alt: "Various Games",
+        text:
+            "A series of little games that helped me learn the basics of Javascript at the Ri7 school." +
+            " The maze is coded in PHP, I couldn't stand the constant refreshes so I used SQL queries. Without realising it, I had just" +
+            " created my first API. Although it was pointless, it was a great learning experience. ",
+        techno: "Javascript, PHP",
+        client: "School Project",
+        type: "game",
+    },
+];
+
+export default projectsData;
